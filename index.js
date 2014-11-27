b890ae4b05647c8a6f2931f7b633678e6e24c90a
Mats Stijlaart <m.stijlaart@avisi.nl>
2014-11-27 19:20:17 +0100

    Removed index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-var Runner = require('./runner');
-var runner = new Runner({ url : 'http://vm267.insight.sbp.avisi.nl:8080/dash'});
-runner.addTask('more', 'weekly-by-customer', 300, require('./tasks/weekly-by-customer'));
-
-runner.start(true);
